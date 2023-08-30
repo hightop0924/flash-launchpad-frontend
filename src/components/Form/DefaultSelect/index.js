1a9719c12c0f4131e9e760013c7ad72e1dec31a4
wholespace214 <wholespace.214@gmail.com>
2023-08-31 04:36:38 +0900

    update the percent flash audit result

--- src/components/Form/DefaultSelect/index.js
+++ src/components/Form/DefaultSelect/index.js
@@ -6,7 +6,7 @@ const DefaultSelect = ({
   ],
 }) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-3">
       {label && <p className="text-white text-[14px] uppercase"> {label} </p>}
       <select className="w-[100%] p-5 bg-[#141414] border border-[#2C2C2C] rounded-[8px] outline-none text-base text-[#86888C]">
         {optons.map((item, index) => (
