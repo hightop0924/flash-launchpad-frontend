1560af49755c01bd21f1d17fcd0eea6f97534718
wholespace214 <wholespace.214@gmail.com>
2023-08-31 02:47:35 +0900

    update the input components

--- src/components/Form/Input/index.js
+++ src/components/Form/Input/index.js
@@ -1,9 +1,12 @@
-const Input = () => {
+const Input = ({ label = "", value = "", onChange = "" }) => {
   return (
-    <input
-      type="text"
-      className="w-[100%] outline-none border border-[#2C2C2C] bg-[#141414] p-5 rounded-lg text-[#86888C] text-base"
-    />
+    <div className="flex flex-col gap-2">
+      {label && <p className="text-white text-[14px] uppercase"> {label} </p>}
+      <input
+        type="text"
+        className="w-[100%] outline-none border border-[#2C2C2C] bg-[#141414] p-5 rounded-lg text-[#86888C] text-base"
+      />
+    </div>
   );
 };
 
