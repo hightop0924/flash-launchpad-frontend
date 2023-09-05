c0edff70e5a5acada5991beef9ab4b299f8c50ae
wholespace214 <wholespace.214@gmail.com>
2023-09-05 23:59:33 +0900

    update the warning components

--- src/components/Alert/Warning/index.js
+++ src/components/Alert/Warning/index.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 //! import icons
 import WarningIcon from "@/assets/icons/warning.svg";
 
-const Warning = ({ children }) => {
+const Warning = ({ children, icon = true }) => {
   return (
     <div className="bg-[#7A4F01]/[0.06] px-6 py-5 flex flex-row gap-3 rounded-[8px] border items-center border-[#B78103]">
-      <Image src={WarningIcon} alt="image" />
+      {icon && <Image src={WarningIcon} alt="image" />}
       {children}
     </div>
   );
