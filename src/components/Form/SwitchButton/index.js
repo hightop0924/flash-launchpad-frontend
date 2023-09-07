4d868d01410b36fadf27d4c109c9be57a96d8d46
wholespace214 <wholespace.214@gmail.com>
2023-09-08 04:16:44 +0900

    add the switchbutton components

--- src/components/Form/SwitchButton/index.js
+++ src/components/Form/SwitchButton/index.js
@@ -1,5 +1,19 @@
-const SwitchButton = () => {
-  return <div></div>;
+//! import icons
+import SwitchIcon from "@/assets/icons/switch.svg";
+import SwitchActiveIcon from "@/assets/icons/switch-active.svg";
+
+import Image from "next/image";
+
+const SwitchButton = ({ onClick = "", check = false }) => {
+  return (
+    <div onClick={() => onClick(!check)} className="cursor-pointer">
+      {check ? (
+        <Image src={SwitchActiveIcon} alt="" />
+      ) : (
+        <Image src={SwitchIcon} alt="" />
+      )}
+    </div>
+  );
 };
 
 export default SwitchButton;
