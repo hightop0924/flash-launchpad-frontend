0579e7aca8945ffbe3858a02ee5f15d8791330b4
wholespace214 <wholespace.214@gmail.com>
2023-09-13 06:39:25 +0900

    staking pool page

---
+++ src/app/staking-pool/page.js
@@ -0,0 +1,24 @@
+"use client";
+import HeaderNav from "@/components/HeaderNav";
+import StakingPoolCard from "@/components/Card/StakingPoolCard";
+import Select from "@/components/Form/RoundedSelect";
+
+const StakingPool = () => {
+  return (
+    <div className="min-h-[1350px]">
+      <HeaderNav />
+      <div className="flex gap-3 justify-end mt-8">
+        <Select label="Networks" />
+        <Select label="Filter By" />
+        <Select label="Sort By" />
+      </div>
+      <div className="mt-[50px] grid grid-cols-4 gap-y-[30px]">
+        {Array.from({ length: 8 }).map((item, index) => (
+          <StakingPoolCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StakingPool;
